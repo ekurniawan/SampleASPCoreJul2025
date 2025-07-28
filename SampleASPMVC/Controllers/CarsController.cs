927f0c96da57b4210767ada1fdfc9d484c1572d8
Erick Kurniawan <erick.kurniawan@gmail.com>
2025-07-28 14:18:40 +0700

    menambahkan inmemory data

--- SampleASPMVC/Controllers/CarsController.cs
+++ SampleASPMVC/Controllers/CarsController.cs
@@ -1,38 +1,23 @@
 using Microsoft.AspNetCore.Mvc;
 using SampleASPMVC.Models;
+using SampleASPMVC.Services;
 
 namespace SampleASPMVC.Controllers
 {
     public class CarsController : Controller
     {
+        private readonly ICar _carService;
+        public CarsController(ICar carService)
+        {
+            _carService = carService;
+        }
+
 
         [HttpGet]
         public IActionResult Index()
         {
-            List<string> cars = new List<string>
-            {
-                "Toyota",
-                "Honda",
-                "Ford",
-                "Chevrolet",
-                "Mitsubishi"
-            };
-            ViewData["username"] = "JohnDoe";
-            ViewData["address"] = "123 Main St, Springfield";
-            ViewBag.City = "Springfield";
-            ViewBag.Country = "USA";
-            ViewBag.Cars = cars;
-
-            List<Car> carList = new List<Car>
-            {
-                new Car { CarID = 1, Model = "Toyota Camry", Type = "Sedan", BasePrice = 300000000, Color = "Red", Stock = 5 },
-                new Car { CarID = 2, Model = "Honda Accord", Type = "Sedan", BasePrice = 320000000, Color = "Black", Stock = 3 },
-                new Car { CarID = 3, Model = "Ford Explorer", Type = "SUV", BasePrice = 450000000, Color = "White", Stock = 2 },
-                new Car { CarID = 4, Model = "Chevrolet Tahoe", Type = "SUV", BasePrice = 500000000, Color = "Silver", Stock = 4 },
-                new Car { CarID = 5, Model = "Mitsubishi Outlander", Type = "SUV", BasePrice = 250000000, Color = "Blue", Stock = 10 }
-            };
-
-            return View(carList);
+            var models = _carService.GetAll();
+            return View(models);
         }
 
         public IActionResult Insert()
--- SampleASPMVC/Program.cs
+++ SampleASPMVC/Program.cs
@@ -1,7 +1,11 @@
+using SampleASPMVC.Services;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
+
+builder.Services.AddScoped<ICar, CarInMemoryServices>();
 
 var app = builder.Build();
 
