639f34badb40f4a69462b61f1f16e7a2bc95a4ca
Erick Kurniawan <erick.kurniawan@gmail.com>
2025-07-29 09:32:35 +0700

    menambakan insert car

--- SampleASPMVC/Services/CarADOServices.cs
+++ SampleASPMVC/Services/CarADOServices.cs
@@ -19,7 +19,32 @@ namespace SampleASPMVC.Services
 
         public Car Create(Car item)
         {
-            throw new NotImplementedException();
+            using (SqlConnection conn = new SqlConnection(GetConnStr()))
+            {
+                try
+                {
+                    string strSql = @"InsertCar";
+                    SqlCommand cmd = new SqlCommand(strSql, conn);
+                    cmd.CommandType = System.Data.CommandType.StoredProcedure;
+                    cmd.Parameters.AddWithValue("@Model", item.Model);
+                    cmd.Parameters.AddWithValue("@Type", item.Type);
+                    cmd.Parameters.AddWithValue("@BasePrice", item.BasePrice ?? 0.0);
+                    cmd.Parameters.AddWithValue("@Color", item.Color);
+                    cmd.Parameters.AddWithValue("@Stock", item.Stock ?? 0);
+                    conn.Open();
+                    var result = Convert.ToInt32(cmd.ExecuteScalar());
+                    item.CarID = result; // Set the CarID to the newly created ID
+                    return item; // Return the created car object
+                }
+                catch (SqlException sqlEx)
+                {
+                    throw new Exception($"Number: {sqlEx.Number} Ket:{sqlEx.Message}");
+                }
+                catch (Exception ex)
+                {
+                    throw new Exception($"Error: {ex.Message}");
+                }
+            }
         }
 
         public void Delete(int id)
@@ -74,7 +99,31 @@ namespace SampleASPMVC.Services
 
         public Car Read(int id)
         {
-            throw new NotImplementedException();
+            using (SqlConnection conn = new SqlConnection(GetConnStr()))
+            {
+                conn.Open();
+                using (SqlCommand cmd = new SqlCommand("GetCarById", conn))
+                {
+                    cmd.CommandType = System.Data.CommandType.StoredProcedure;
+                    cmd.Parameters.AddWithValue("@CarID", id);
+                    using (SqlDataReader reader = cmd.ExecuteReader())
+                    {
+                        if (reader.Read())
+                        {
+                            return new Car
+                            {
+                                CarID = Convert.ToInt32(reader["CarID"]),
+                                Model = reader["Model"].ToString(),
+                                Type = reader["Type"].ToString(),
+                                BasePrice = Convert.ToDouble(reader["BasePrice"]),
+                                Color = reader["Color"].ToString(),
+                                Stock = Convert.ToInt32(reader["Stock"])
+                            };
+                        }
+                        return null; // or throw an exception if not found
+                    }
+                }
+            }
         }
 
         public Car Update(Car item)
